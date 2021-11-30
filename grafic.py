bdd67f6f1fd4463557e5d9bd0a89e632da50dd47
Nil PAV ETSETB <nil.cruz@estudiantat.upc.edu>
2021-11-30 16:10:23 +0100

    Fitxers Actualitzats

--- grafic.py
+++ grafic.py
@@ -2,32 +2,29 @@ import numpy as np
 import soundfile as sf
 import matplotlib.pyplot as plt
 import scipy.signal as sig
-file_in='home/PAV/P3/pitch_db/train/rl002.wav'
+file_in='pitch_db/train/rl002.wav'
 x,fm=sf.read(file_in)
 
-#Time vector
 samples = []
 for i in range(0,len(x)):
     samples.append(i/fm)
 
-#Autocorrelation
 autocorrelation=np.correlate(x,x,"same")
 m = np.arange(len(autocorrelation))
 peaks = sig.find_peaks(autocorrelation)
 [a1,a2]=np.split(autocorrelation,2)
 
-#Plot generation
 plt.subplot(2,1,1)
-plt.plot(samples,x,lineWidth=0.5)
-plt.title('30 ms of voiced frames')
+plt.plot(samples,x,linewidth=0.5)
+plt.title('Voiced frame')
 plt.grid(True)
 plt.xlabel('Time[s]')
 plt.ylabel('Waveform')
-plt.xlim(0.09,0.12)
+plt.xlim(0.35,0.38)
 plt.subplot(2,1,2)
-plt.plot(a2,lineWidth=0.5)
+plt.plot(a2,linewidth=0.5)
 plt.xlabel('Samples')
 plt.ylabel('Autocorrelation')
 plt.xlim(0,200)
 plt.grid(True)
-plt.show()
+plt.show(block=True)
--- grafic_pitch.py
+++ grafic_pitch.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
-file_in_1='rl002.txt'
-file_in_2='home/PAV/P3/pitch_db/train/rl002.f0' 
+file_in_1='pitch_db/train/rl002.f0ref'
+file_in_2='pitch_db/train/rl002.f0' 
 
 wavesurfer = np.loadtxt(file_in_1,skiprows=0)
 estimacion = np.loadtxt(file_in_2,skiprows=0)
@@ -13,11 +13,11 @@ for i in range(0,len(estimacion)):
     samples.append(i*dm)
 
 
-plt.plot(samples,estimacion,'g',lineWidth=1,label='Pitch detector')
-plt.plot(samples,wavesurfer,'r',lineWidth=1.5,label='Wavesurfer pitch detector')
+plt.plot(samples,estimacion,'b',linewidth=1,label='Pitch detector')
+plt.plot(samples,wavesurfer,'r',linewidth=1.5,label='Wavesurfer pitch detector')
 plt.title('Pitch Estimation')
 plt.xlabel('Time[s]')
 plt.ylabel('Frequency[Hz]')
 plt.xlim(0,1.24)
 plt.legend(loc='upper right')
-plt.show()
+plt.show(block=True)
